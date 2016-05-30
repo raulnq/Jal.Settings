21200c30557370ef34b3bbb5aba0cea0d76d4d16
Raul Naupari <raul.naupari@cignium.com>
2016-05-30 16:19:16 -0500

    refactoring

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
-packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Settings\Jal.Factory.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Settings.Nuget
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Settings\Jal.Settings.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Settings.Nuget
 
-packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Settings.Installer\Jal.Factory.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Settings.Nuget
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Settings.Installer\Jal.Settings.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Settings.Nuget
 
 pause;
