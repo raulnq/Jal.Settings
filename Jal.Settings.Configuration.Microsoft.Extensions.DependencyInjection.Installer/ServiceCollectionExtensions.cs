2af0777f13d68ed63cc5f58316422cc28b961834
rnaupari <raul.naupari@cignium.com>
2019-11-07 10:54:50 -0500

    lib updated+DI net installer

---
+++ Jal.Settings.Configuration.Microsoft.Extensions.DependencyInjection.Installer/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Jal.Settings.Configuration.Impl;
+using Jal.Settings.Interface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jal.Settings.Configuration.Microsoft.Extensions.DependencyInjection.Installer
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFactory(this IServiceCollection servicecollection, IConfiguration configuration)
+        {
+            servicecollection.AddSingleton<ISectionExtractor, SectionExtractor>();
+
+            servicecollection.AddSingleton<ISettingsExtractor, SettingsExtractor>();
+
+            servicecollection.AddSingleton<IConfiguration>(x => configuration);
+
+            return servicecollection;
+        }
+    }
+}
